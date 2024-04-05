380ec2253ec724c3100caf072d238e99d7ca84a0
brianuribe6 <brian.uribe@outlook.com>
2024-04-05 14:28:16 -0400

    Respond to ping requests

--- app/server.go
+++ app/server.go
@@ -7,14 +7,34 @@ import (
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err)
+			continue
+		}
+		handleClient(conn);
+	}
+}
+
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	buff := make([]byte, 1024)
+	n, err := conn.Read(buff)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println("Failed to read message:", err);
 		os.Exit(1)
 	}
+	fmt.Println(string(buff[:n]));
+	msg := []byte("+PONG\r\n")
+	conn.Write(msg)
+
 }
