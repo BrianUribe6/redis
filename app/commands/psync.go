1aca0193c41b317f97a3ff8c4c3fcffacb964373
brianuribe6 <brian.uribe@outlook.com>
2024-04-22 03:24:14 -0400

    Remove, file based RDB implementation. (Codecrafters test suite is inconsistently passing/failing the tests)

--- app/commands/psync.go
+++ app/commands/psync.go
@@ -1,14 +1,11 @@
 package command
 
 import (
-	"bufio"
+	"encoding/base64"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"os"
 
-	"github.com/codecrafters-io/redis-starter-go/app/rdb"
 	"github.com/codecrafters-io/redis-starter-go/app/resp"
 	"github.com/codecrafters-io/redis-starter-go/app/store"
 )
@@ -26,39 +23,26 @@ func (cmd *PSYNCCommand) Execute(con net.Conn) {
 
 	// 2. Read the file dump of the database
 	log.Println("Loading RDB...")
-	rdb.CreateEmpty()
 
-	rdbFile, err := os.Open(rdb.FILENAME)
-	if err != nil {
-		log.Fatal("Failed to open RDB:", err)
-	}
-	defer rdbFile.Close()
-
-	rdbFileInfo, err := rdbFile.Stat()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	reader := bufio.NewReader(rdbFile)
-
+	data := CreateEmptyRDB()
 	// 3. Format it as a RESP file syntax and send it in CHUNKS
 	// RESP Syntax for sending files is $<length_of_file>\r\n<contents_of_file>
-	file := []byte(fmt.Sprintf("$%d\r\n", rdbFileInfo.Size()))
+	file := []byte(fmt.Sprintf("$%d\r\n", len(data)))
 
-	data := make([]byte, 4096)
-	for {
-		n, err := reader.Read(data)
-		if err == io.EOF {
-			break
-		}
-		file = append(file, data[:n]...)
-	}
+	file = append(file, data...)
+
 	con.Write(file)
 
+	log.Printf("Syncronization with replica %s succeeded", con.RemoteAddr().String())
+}
+
+func CreateEmptyRDB() []byte {
+	str := "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog=="
+	data, err := base64.StdEncoding.DecodeString(str)
 	if err != nil {
-		log.Println("Sync failed:", err.Error())
-		return
+		log.Panic(err)
 	}
 
-	log.Printf("Syncronization with replica %s succeeded", con.RemoteAddr().String())
+	// os.WriteFile(FILENAME, data, 0666)
+	return data
 }
